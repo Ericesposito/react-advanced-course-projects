3f838ef17c961d5fdbb9e0aad5c998420b5f2597
Ericesposito <esposito.eric.andre@gmail.com>
2024-05-14 22:09:52 -0400

    finished converting from vanilla redux to redux toolkit for establishing slices of state

--- redux/src/store/index.js
+++ redux/src/store/index.js
@@ -1,36 +1,13 @@
-import { createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 
-const initialState = { counter: 0, showCounter: true };
+import counterReducer from './counter';
+import authReducer from './auth';
 
-const counterReducer = (state = initialState, action) => {
-  // if (action.type === 'increment') {
-  //   return { ...state, counter: state.counter + 1 };
-  // }
-  if (action.type === 'increment') {
-    return {
-      counter: state.counter + 1,
-      showCounter: state.showCounter,
-    };
-  }
-
-  if (action.type === 'increase') {
-    return { ...state, counter: state.counter + action.amount };
-  }
-
-  if (action.type === 'decrement') {
-    return { ...state, counter: state.counter - 1 };
-  }
-
-  if (action.type === 'toggle') {
-    return {
-      showCounter: !state.showCounter,
-      counter: state.counter,
-    };
-  }
-
-  return state;
-};
-
-const store = createStore(counterReducer);
+const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+    auth: authReducer,
+  },
+});
 
 export default store;
