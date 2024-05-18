d4f31cc242bcbf214b57ead9cf4fc4b312cc20be
Ericesposito <esposito.eric.andre@gmail.com>
2024-05-18 15:48:43 -0400

    typescript refresher

---
+++ typescript-react/basics.ts
@@ -0,0 +1,44 @@
+// Primitives: number, string, boolean, null, undefined, symbol
+// More complex types: arrays, objects
+// Function types, parameters
+
+// Primitives
+
+let age: number;
+
+age = 30;
+
+let userName: string;
+
+userName = 'Eric';
+
+let isHired;
+
+isHired = true;
+
+// More complex types
+
+let hobbies: string[];
+
+hobbies = ['Yoga', '3D Printing', 'Cinema'];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: 'Eric',
+  age: 30,
+};
+
+let people: {
+  name: string;
+  age: number;
+}[];
+
+people = [person];
+
+let course: string = 'Ract - The Complete Guide';
+
+course = 201;
